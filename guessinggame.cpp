d9b29f67ea299b1bf96c7f12458c115f12c7949c
U-BSD\493914 <493914@SHS-CYBF6X3.beaverton.k12.or.us>
2025-09-05 10:16:14 -0700

    5th

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -1,8 +1,58 @@
+/*
+Emilio Vasquez-Pettit
+09/05/2025
+Citations:
+1. 
+*/
 #include <iostream>
+#include <random>
 using namespace std;
-
 int main()
 {
-  cout << "Hello\n";
-  cout << "Test";
+  
+  
+  int number = 10;
+  int turns;
+  
+  while (true)
+    {
+      int guess;
+  while (true)
+    {
+      cout << "Enter a number from 0 to 100 inculsive: ";
+      cin >> guess;
+      if (100 < guess < 0)
+	{
+	  cout << "Number out of range, please choose a different number.";
+	  continue;
+	}
+      
+      cin >> guess;
+      if (100 < guess < 0)
+	{
+	  cout << "Number out of range, guess again.";
+	  turns += 1;
+	  continue;
+	}
+      if (guess > number)
+	{
+	  cout << "Number too high, guess again";
+	  turns += 1;
+	  continue;
+	}
+      if (guess < number)
+	{
+	  cout << "Number too low, guess again";
+	  turns += 1;
+	  continue;
+	}
+      if (guess = number)
+	{
+	  cout << "You guessed correctly, you win!";
+	  turns += 1;
+	  cout << "You took " << turns << " turns.";
+	  break;
+	}
+    }
+  return 0;
 }
