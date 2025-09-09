91dbd8b5e383d08db9164bd9333adb87cbbe9143
U-BSD\493914 <493914@SHS-CYBF6X3.beaverton.k12.or.us>
2025-09-09 10:18:51 -0700

    save1

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -7,51 +7,65 @@ Citations:
 #include <iostream>
 #include <random>
 using namespace std;
+
+int Guessing()
+  {
+    int guess;
+    bool guessvalid = false;
+    while (guessvalid == false)
+      {
+	cout << "Enter an integer from 0 to 100 inculsive: ";
+	cin >> guess;
+	if (100 < guess < 0)
+	  {
+	    cout << "Number out of range, please choose a different number.";
+	  }
+	if (101 > guess > -1)
+	  {
+	    guessvalid = true;
+	  }
+      }
+    return 0;
+  }
+
 int main()
 {
   
   
   int number = 10;
   int turns;
+  int guess;
+  bool guessvalid = false;
+  bool guessright = false;
+  int guessing;
   
-  while (true)
+  while (guessright == false)
     {
-      int guess;
-  while (true)
-    {
-      cout << "Enter a number from 0 to 100 inculsive: ";
-      cin >> guess;
-      if (100 < guess < 0)
-	{
-	  cout << "Number out of range, please choose a different number.";
-	  continue;
-	}
       
-      cin >> guess;
       if (100 < guess < 0)
 	{
 	  cout << "Number out of range, guess again.";
 	  turns += 1;
-	  continue;
+	  Guessing();
 	}
       if (guess > number)
 	{
 	  cout << "Number too high, guess again";
 	  turns += 1;
-	  continue;
+	  Guessing();
 	}
       if (guess < number)
 	{
 	  cout << "Number too low, guess again";
 	  turns += 1;
-	  continue;
+	  Guessing();
 	}
-      if (guess = number)
+      if (guess == number)
 	{
 	  cout << "You guessed correctly, you win!";
 	  turns += 1;
 	  cout << "You took " << turns << " turns.";
-	  break;
+	  guessright = true;
 	}
     }
   return 0;
