226b7b1eb49c99905c23c5e6174a63cf5ad26824
U-BSD\493914 <493914@SHS-CYBF6X3.beaverton.k12.or.us>
2025-09-15 12:58:56 -0700

    start

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,1 +1,17 @@
-//test case
+/*
+Emilio Vasquez-Pettit
+09/15/2025
+Palindrome
+
+This program reads a series of characters (max of 80), removes all punctuation and spaces, and returns whether or not the stripped input is a palindrome or not (same read backwards as forwards).
+
+Citations:
+
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  
+}
