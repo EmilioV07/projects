8b577f61365d05142fbbff9282544c521dd686c6
U-BSD\493914 <493914@SHS-CYBF6X3.beaverton.k12.or.us>
2025-10-08 10:03:02 -0700

    3 function

--- ttt.cpp
+++ ttt.cpp
@@ -13,9 +13,39 @@ Citations:
 
 using namespace std;
 
-int checkwin(char board[3][3], int turn)
+bool checkwin(char board[3][3], int turn)//win detection, iterates through rows and columns, then checks both diagonals manually
 {
-  return 0;
+  //check horizontals and verticals
+  for (int i = 0; i < 3; i++)
+    {
+      if (board[i][0] == board[i][1] == board[i][2] && board[i][0] != '-')
+	{
+	  cout << board[i][0] << " wins!" << endl;
+	  return true;
+	}
+      else if (board[0][i] == board[1][i] == board[2][i] && board[0][i] != '-')
+	{
+	  cout << board[0][i] << " wins!" << endl;
+	  return true;
+	}
+    }
+  //check diagonals
+  else if (board[0][0] == board[1][1] == board[2][2] ** board [0][0] != '-')
+    {
+      cout << board[0][0] << " wins!" << endl;
+      return true;
+    }
+  else if (board[0][2] == board[1][1] == board[2][0] ** board [0][2] != '-')
+    {
+      cout << board[0][2] << " wins!" << endl;
+      return true;
+    }
+  //check tie
+  
+  /*
+  if "-" not in board[0][0] and "-" not in board[0][1] and "-" not in board[0][2] and "-" not in board[1][0] and "-" not in board[1][1] and "-" not in board[1][2] and "-" not in board[2][0] and "-" not in board[2][1] and "-" not in board[2][2] and winner == False:
+  */
+  return false;
 }
 int print_board(char board[3][3], int turn)
   {
@@ -36,6 +66,12 @@ int print_board(char board[3][3], int turn)
     return 0;
   }
 
+bool isvalidmove(row, col)//checks if the player's move choice is valid (not overwriting an existing move of either player)
+{
+  //add range check
+  return board[row][col] == '-';//copilot simplification suggestion of an if and else compared to simply returning bool result of existing comparison
+}
+
 int main()
 {
   int turn = 1;
@@ -44,79 +80,24 @@ int main()
 
   //function declarations
   int print_board(char board[3][3], int turn);
-  int checkwin(char board[3][3], int turn);
-  //setting up the board
-  cout << "Welcome to Tic Tac Toe" << endl;
-  print_board(board, turn);
+  bool checkwin(char board[3][3], int turn);
+  bool isvalidmove(row, col);
   
+  //setting up the board
+  while (playing == true)
+    {
+      turn = 1;
+      winner = false;
+      cout << " Welcome to Tic Tac Toe" << endl;
+      print_board(board, turn);
+      taketurn(board, turn);
+    }
 }
 
+
+
 /*
-#Copy your functions from the previous activities here:
-#Global Variables
-board = [["-","-","-"], ["-","-","-"], ["-","-","-"]]#row -> column
-turn = 1
-winner = False
-#Functions
-#copy and paste your print_board function definition here!
-def check_win():
-    global board
-    global winner
-    for i in range(3):
-        #check horizontal and change win counter and clear board accordingly
-        if board[i][0] == board[i][1] == board[i][2] and board[i][0] != "-":
-            if board[i][0] == "O":
-                print("O wins!")
-                return True
-            elif board[i][0] == "X":
-                print("X wins!")
-                return True
-        #check vertical and change win counter and clear board accordingly
-        if board[0][i] == board[1][i] == board[2][i] and board[0][i] != "-":
-            if board[0][i] == "O":
-                print("O wins!")
-                return True
-            elif board[0][i] == "X":
-                print("X wins!")
-                return True
-    #check diagonals and change win counter and clear board accordingly
-    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != "-":
-        if board[0][0] == "O":
-            print("O wins!")
-            return True
-        elif board[0][0] == "X":
-            print("X wins!")
-            return True
-    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != "-":
-        if board[0][2] == "O":
-            print("O wins!")
-            return True
-        elif board[0][2] == "X":
-            print("X wins!")
-            return True
-    #check tie and clear board accordingly
-    if "-" not in board[0][0] and "-" not in board[0][1] and "-" not in board[0][2] and "-" not in board[1][0] and "-" not in board[1][1] and "-" not in board[1][2] and "-" not in board[2][0] and "-" not in board[2][1] and "-" not in board[2][2] and winner == False:
-        print("tie!")
-def print_board():
-    print("")
-    global board
-    global turn
-    if turn == -1:
-        print("O's turn")
-    elif turn == 1:
-        print("X's turn")
-    print("  "+ "0" + " " + "1" + " " + "2")
-    for i in range(3):
-        print(str(i) + " " + board[i][0] + " " + board[i][1] +  " " + board[i][2])
-#returns true if a row,col on the board is open
-def is_valid_move(row, col):
-    global board
-    while True:
-        if board[row][col] != "-":
-            return False
-        elif board[row][col] == "-":
-            return True
-            break#backup
+
 #places player on row,col on the board
 def place_player(row, col):
     global turn
