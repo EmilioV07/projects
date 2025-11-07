aa0ac6428023e4a73db93c4ae3a83c95ae91c659
U-BSD\493914 <493914@SHS-CYBF6X3.beaverton.k12.or.us>
2025-11-07 09:46:01 -0800

    save10

--- c.cpp
+++ c.cpp
@@ -1,9 +1,20 @@
 /*
 Emilio Vasquez-Pettit
+11/07/2025
 
+This program
+
+Citations:
+1. 
 */
 
 #include <iostream>
-
+#include <vector>
+#include <cmath>
 using namespace std;
-
+int main()
+  {
+    
+    
+    return 0;
+  }
