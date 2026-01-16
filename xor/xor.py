828c6eeaefb331e6ab6f1f8a42cd21f2db397b49
U-BSD\493914 <493914@SHS-CYBF6X3.beaverton.k12.or.us>
2026-01-16 13:42:57 -0800

    xor16done

--- xor/xor.py
+++ xor/xor.py
@@ -137,7 +137,6 @@ def brk(bytelines, keysize): #breaks into keysize sizes chunks AND transposes (1
 	chunklist = [] #CHUNKLIST: List of broken segments BEFORE transposing
 	for i in range(0, len(bytelines), keysize):
 		chunklist.append(bytelines[i:i+keysize])
-	print(chunklist)
 	return chunklist
 def brk2(bytelines, keysizes): #second brk function specifically made to break based on top 3 keysizes
 	chunklist1 = []
@@ -152,7 +151,6 @@ def brk2(bytelines, keysizes): #second brk function specifically made to break b
 		chunklist2.append(bytelines[j:j+keysizes[1]])
 	for j in range(0,len(bytelines), keysizes[2]):
 		chunklist3.append(bytelines[j:j+keysizes[2]])
-	print(chunklist2)
 	for chunk in chunklist1:
 		for index in range(len(chunk)):
 			pieceslist1[index].append(chunk[index])
@@ -165,25 +163,31 @@ def brk2(bytelines, keysizes): #second brk function specifically made to break b
 	return pieceslist1, pieceslist2, pieceslist3
 def findkey(bytelines, pieces):
 	result = []
-	keycycle = itertools.cycle([i]) #sets cycle for each line individually
-	print(''.join(result))rslt = [] #works as buffer for holding variables to manipulate
-	rsltcmp = [] #stores final product for later comparison
-	score = 0 #holds score
-	cmnchars = "etaoin shrdlu" #common chars provided on challenge page
-	for i in range(3):
-		for i in range(32, 127): #iterates through all printable ASCII characters (chr(i))
-			key = chr(i).encode() * len(pieceslist[i]) #converts ASCII character to bytes for later XOR
-			msg = a ^ b for a, b in zip(pieceslist[i], key) #XORs bytes of original hex with character
-			dmsg = msg.decode(errors="ignore").lower() #converts the message to a lowercase string for comparison
-			rslt.append((chr(i), dmsg)) #prints xor character and output against encoded string bytes
-			for j in rslt[0][1]: #scores every xor option for english detection
-			
-			rslt.append(score) #stores score in print buffer
-			rsltcmp.append((chr(i),dmsg,score))
-			score = 0
-			print(rslt)
-			rslt.clear()
-	return None
+	foundkeys = []
+	for piece in pieces:
+		pieceresult = []
+		for block in piece:
+			temp = []
+			for i in range(32, 127): #iterates through all printable ASCII characters (chr(i))
+				key = chr(i).encode() * len(block) #converts ASCII character to bytes for later XOR
+				msg = [a ^ b for a, b in zip(block, key)] #XORs bytes of original hex with character
+				dmsg = bytes(msg).decode(errors="ignore").lower() #converts the message to a lowercase string for comparison
+				temp.append((chr(i), engtest(dmsg)))
+			temp.sort(key=lambda x: x[1][0], reverse=True) #sort for best score on top
+			best=temp[0]
+			pieceresult.append(best)
+		result.append(pieceresult)
+
+		key4piece = ''.join([char for char, score in pieceresult])
+		foundkeys.append(key4piece)
+	#for i in foundkeys:
+		#print(i)
+	bestkey = []
+	for i in foundkeys:
+		bestkey.append(engtest(i))
+	bestkey.sort(key=lambda x: x[0], reverse=True)
+	print("\nThe Key Is: '" + bestkey[0][1] + "'\n")
+	return bestkey[0][1]
 def kcontend(bytelines): #finds the keysize using ham, RETURNS SMALLEST 3 NORMALIZED HAMMING DISTANCES AND CORRESPONDING KEYSIZE
 	all = []
 	#first, get sample pairs of every keysize chunk (first 2 keysize sized chunks for every keysize)
@@ -193,12 +197,10 @@ def kcontend(bytelines): #finds the keysize using ham, RETURNS SMALLEST 3 NORMAL
 		broken = brk(bytelines, keysize) #makes a chunklist for given i keysize
 		temp.append((broken[0], broken[1], broken[3], broken[4], keysize)) #adds first 4 elements of new chunklist into temp for hamming
 		broken.clear()
-	#print(temp)
 	#then, get their hamming distance P.S. ELEMENTS ALREADY PAIRED
 	for pair in temp:
 		all.append(ham(pair[0],pair[1],pair[2],pair[3],pair[4]))
 	all.sort(key=lambda x: x[0]) #sorts by smallest to largest normalized hamming distances
-	#print(all)
 	return all
 def engtest(sample): #scores input in english likeness
 	cmnchars = "etaoin shrdlu ETAOIN SHRDLU"
@@ -212,7 +214,7 @@ def engtest(sample): #scores input in english likeness
 			score += 3
 		if j in ")(*&^%$#@~+_{}[]\|<>`":
 			score -=2
-	return score
+	return score, sample
 def ham(s1, s2, s3, s4, keysize): #hamming distance of 2 BYTE inputs EDIT: CHANGE TO SECOND HAMMING OPTION
 	sum1, sum2, sum3, sum4, sum5, sum6 = 0, 0, 0, 0, 0, 0
 	for a, b in zip(s1, s2):
@@ -229,10 +231,25 @@ def ham(s1, s2, s3, s4, keysize): #hamming distance of 2 BYTE inputs EDIT: CHANG
 		sum6 += (bin(a^b).count("1"))/keysize
 	return sum([sum1, sum2, sum3, sum4, sum5, sum6])/6, keysize #returns the iterated normalized sum of differing bytes in the strings
 
+def unxor(bytelines, key):
+	result = []
+	if isinstance(bytelines, (bytes,bytearray)):
+		bytelines = [bytelines] #copilot debug suggestion for wrong filetype
+	for byteline in bytelines:
+		keycycle = itertools.cycle(key.encode()) #sets cycle for each line individually
+		xdline = []
+		for char in byteline: #cycles through line once
+			xdline.append(char ^ next(keycycle)) #XORs the one character of the line by current character of key
+		decodedxdline = bytes(xdline).decode(errors="ignore")
+		result.append(decodedxdline) #adds line to list as a seperate list element
+	print("And the decoded message is: \n")
+	print(''.join(result))
+
+
+
 def main(bytelines):
 	all = kcontend(bytelines)
-	print(all[0],all[1],all[2]) #prints top three contender keysizes and their correlation
 	top3 = brk2(bytelines, (all[0][1],all[1][1],all[2][1]))
-	print(top3)
 	key = findkey(bytelines, top3)
+	unxor(bytelines, key)
 main(bytelines)
